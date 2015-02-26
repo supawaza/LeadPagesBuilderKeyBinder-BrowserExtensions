3dc06f07f09aae0877448fb90fea25e4f4a7a8c7
Mon Lu <mon@ave81.com>
2015-02-26 09:58:23 -0600

    Updated the repos name for rawgit

--- chrome/keysbinder.js
+++ chrome/keysbinder.js
@@ -3,7 +3,7 @@ var checkInterval;
 checkInterval = setInterval(function(){
 	if(window.App !== 'undefined'){
 		var el = document.createElement('script');
-		el.setAttribute('src', 'https://cdn.rawgit.com/LeadPages/builderBookmarklet/master/builderKeybinding.js');
+		el.setAttribute('src', 'https://cdn.rawgit.com/LeadPages/LeadPagesBuilderBookmarklet/master/builderKeybinding.js');
 		document.head.appendChild(el);
 
 		clearInterval(checkInterval);
